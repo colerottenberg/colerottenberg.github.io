0e3238cc98a8d52a60652bc55b59a3b4f9a9ba81
Cole Rottenberg <Cole.rottenberg@gmail.com>
2024-11-08 13:59:14 -0500

    adding config for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
 import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
 import icon from "astro-icon";
-import { defineConfig } from 'astro/config';
-
-import vercel from "@astrojs/vercel/serverless";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon(), compress()],
-  output: "server",
-  adapter: vercel()
+  site: "https://colerottenberg.github.io",
 });
